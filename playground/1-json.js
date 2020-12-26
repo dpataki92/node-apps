26184542114ac1854e9137c9e0b33cbc90a87b6e
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-26 19:32:19 +0000

    add example - storing data with JSON

---
+++ playground/1-json.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+/* const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday'
+}
+
+const bookJSON = JSON.stringify(book);
+fs.writeFileSync('1-json.json', bookJSON); */
+
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+
+const data = JSON.parse(dataJSON);
+data.name = "Daniel Pataki";
+data.age = 28;
+
+const newDataJSON = JSON.stringify(data);
+fs.writeFileSync('1-json.json', newDataJSON);
