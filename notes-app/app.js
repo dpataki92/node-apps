7ca012806925b10a3d7dcdda06c370134d21637d
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-26 16:17:19 +0000

    add examples - using external npm package(chalk)

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,9 @@
 const validator = require('validator');
+const chalk = require('chalk');
 
+const log = console.log;
 const getNotes = require('./notes.js');
 
-console.log(getNotes());
+log(chalk.blue.green.inverse(getNotes()));
 
-console.log(validator.isURL('https://something.com'));
+log(chalk.red(validator.isURL('https:/something.com')));
