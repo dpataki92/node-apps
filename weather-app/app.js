60b83ea803b38f8054a7d169f3daf5b9ae56cd7e
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-28 18:03:31 +0000

    add initial http request for weatherstack api with request module (weather app)

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,11 @@
-console.log('Starting');
+const request = require('request');
+const chalk = require('chalk');
 
+const url = "http://api.weatherstack.com/current?access_key=78c970c75096e0c22d50f66617df8dca&query=37.8267,-122.4233&units=f";
 
-setTimeout(() => {
-    console.log("2 second timer");
-}, 2000)
-
-setTimeout(() => {
-    console.log("0 second time");
-}, 0)
-
-console.log('Stopping');
+request({url: url, json: true}, (err, resp) => {
+    resp.body.current.weather_descriptions.forEach(e => {
+        console.log(chalk.green(e));
+    });
+    console.log(`It is currently ${resp.body.current.temperature} degrees out. It feels like ${resp.body.current.feelslike} degrees.`);
+})
