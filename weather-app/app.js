527c4ba634a639ca8d10bf1c7e2a34b342014a7b
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-28 15:39:04 +0000

    setup weather-app folder with example for asynchronous logic

---
+++ weather-app/app.js
@@ -0,0 +1,12 @@
+console.log('Starting');
+
+
+setTimeout(() => {
+    console.log("2 second timer");
+}, 2000)
+
+setTimeout(() => {
+    console.log("0 second time");
+}, 0)
+
+console.log('Stopping');
