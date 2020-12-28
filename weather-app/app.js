bd51bc6af74bfea3665f0e2b33e6af501a0bc5ec
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-28 18:57:23 +0000

    add http request for mapbox api to get geodata (weather app)

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,3 +9,12 @@ request({url: url, json: true}, (err, resp) => {
     });
     console.log(`It is currently ${resp.body.current.temperature} degrees out. It feels like ${resp.body.current.feelslike} degrees.`);
 })
+
+const geoCodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZHBhdGFraTkyIiwiYSI6ImNrajh3Y2M0NzZ2bzYyc2xiazdpYTFwdmgifQ.qpDETULHCWqm89WCMkgQWA";
+
+request({url: geoCodeURL, json: true}, (err, resp) => {
+    const geoData = resp.body.features[0].center;
+    const name = resp.body.features[0].place_name;
+    
+    console.log(chalk.red(name), chalk.green(`Latitude: ${geoData[1]}, Longitude: ${geoData[0]}`));
+})
