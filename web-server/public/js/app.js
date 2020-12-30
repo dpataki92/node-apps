a942d54d9e6df3f46f314957d1b35dbf2745f551
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-30 22:30:34 +0000

    add search form with eventlistener to allow searching for locations

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,21 @@
 console.log('Client side js file is loaded');
 
-fetch('http://localhost:3000/weather?address=Boston')
-.then((resp) => {
-    resp.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        }
-        else {
-            console.log(data.location, data.forecast);
-        }
+
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:3000/weather?address=${search.value}`)
+    .then((resp) => {
+        resp.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            }
+            else {
+                console.log(data.location, data.forecast);
+            }
+        })
     })
-})
+});
