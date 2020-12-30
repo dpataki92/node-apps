3a6cc822827c7d3ad709305821a2512bd43ed9b6
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-30 22:10:44 +0000

    add example client-side fetch request

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side js file is loaded');
+
+fetch('http://localhost:3000/weather?address=Boston')
+.then((resp) => {
+    resp.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        }
+        else {
+            console.log(data.location, data.forecast);
+        }
+    })
+})
