ded8b6fba9eba47608a37aa1c2274e652e4118e8
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-30 15:51:27 +0000

    add dynamic templating with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,8 +8,20 @@ console.log(path.join(__dirname, '../public'));
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
+app.get("", (req, res) => {
+    res.render("index", {title: "Weather App"});
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {name: "Daniel Pataki", img: "/img/robot.png"});
+});
+
+app.get("/help", (req, res) => {
+    res.render("help", {name: "Daniel Pataki", email: "danielpataki@gmail.com"});
+});
 
 app.get('/weather', (req, res) => {
     res.send({
