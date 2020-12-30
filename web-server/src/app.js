a6c802957304d92e8d70ea1ba1cec205d9a426bf
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-30 14:52:39 +0000

    add get routes with json and html responses

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,28 @@ const express = require("express");
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello, express!');
+    res.send('<h1>Hello, express!</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help page');
+    res.send([{
+        name: "Daniel",
+        age: 28
+    }, {
+        name: "Bonnie",
+        age: 37
+    }]);
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.send('<h1>About page</h1>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Current weather');
+    res.send({
+        forecast: "It is cloudy",
+        location: "London"
+    });
 });
 
 app.listen(3000, () => {
