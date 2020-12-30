ffcad94293fa991d827cef8d7db68a52a5addc79
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-30 16:54:58 +0000

    add partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,32 +1,33 @@
 const path = require('path');
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
-console.log(__dirname);
-console.log(path.join(__dirname, '../public'));
 
 // define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 //setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
-    res.render("index", {title: "Weather App"});
+    res.render("index", {title: "Weather App", name: "Daniel Pataki"});
 });
 
 app.get("/about", (req, res) => {
-    res.render("about", {name: "Daniel Pataki", img: "/img/robot.png"});
+    res.render("about", {title: "Weather App", name: "Daniel Pataki", img: "/img/robot.png"});
 });
 
 app.get("/help", (req, res) => {
-    res.render("help", {name: "Daniel Pataki", email: "danielpataki@gmail.com"});
+    res.render("help", {title: "Weather App", name: "Daniel Pataki", email: "danielpataki@gmail.com"});
 });
 
 app.get('/weather', (req, res) => {
