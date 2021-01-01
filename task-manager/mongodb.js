c334ce88e47892765b7d5ba66ceb0497412748d6
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-01 19:55:05 +0000

    add examples to update documents with mongodb

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -10,31 +10,24 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
 
     const db = client.db(databaseName);
 
-/*     db.collection("users").findOne({_id: new ObjectID("5fef67bd0335640d1fa2b369")}, (error, user) => {
-        if (error) return console.log("Unable to fetch user.");
+/*     db.collection("users").updateOne({_id: ObjectID("5fef5dc3fcb02b0c95ebb43b")}, {
+        $inc: {
+            age: 1
+        }
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
+    }) */
 
-        console.log(user);
-    }); */
-
-/*     db.collection("users").find({age: 27}).toArray((error, users) => {
-        console.log(users);
-    });  
-
-    db.collection("users").find({age: 27}).count((error, count) => {
-        console.log(count);
-    });  */
-
-
-    db.collection("tasks").findOne({_id: ObjectID("5fef667643b60b0cf560524c")}, (error, user) => {
-        if (error) return console.log("Unable to fetch user");
-
-        console.log(user);
-    })
-
-    db.collection("tasks").find({completed: false}).toArray((error, users) => {
-        if (error) return console.log("Unable to fetch user");
-
-        console.log(users);
+    db.collection("tasks").updateMany({completed: false}, {
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
     })
 
 });
