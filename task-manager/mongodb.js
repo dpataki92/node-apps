c1b4230c2f9c23ba46365291fd6dae19398ccaaa
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-01 18:49:22 +0000

    add examples - read data from database with mongodb

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -5,56 +5,36 @@ const {MongoClient, ObjectID} = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-const id = new ObjectID();
-console.log(id.id.length)
-console.log(id.toHexString().length)
-
 MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
     if (error) {return console.log("Unable to connnect to database!")}
 
     const db = client.db(databaseName);
 
-/*     db.collection("users").insertOne({
-        _id: id,
-        name: "Borris",
-        age: 35
-    }, (error, result) => {
-        if (error) return console.log("Unable to insert user!");
-        console.log(result.ops)
-    })  */
+/*     db.collection("users").findOne({_id: new ObjectID("5fef67bd0335640d1fa2b369")}, (error, user) => {
+        if (error) return console.log("Unable to fetch user.");
+
+        console.log(user);
+    }); */
+
+/*     db.collection("users").find({age: 27}).toArray((error, users) => {
+        console.log(users);
+    });  
+
+    db.collection("users").find({age: 27}).count((error, count) => {
+        console.log(count);
+    });  */
 
 
-/*     db.collection("users").insertMany([
-        {
-            name: "Tamas",
-            age: 58
-        },
-        {
-            name: "Bonnie",
-            age: 38
-        }
-    ], (error, result) => {
-        if (error) return console.log("Unable to insert documents!");
+    db.collection("tasks").findOne({_id: ObjectID("5fef667643b60b0cf560524c")}, (error, user) => {
+        if (error) return console.log("Unable to fetch user");
 
-        console.log(result.ops);
-    }) */
+        console.log(user);
+    })
 
-/*     db.collection("tasks").insertMany([
-        {
-            description: "Online meeting on January 2, 2pm CET",
-            completed: false
-        },
-        {
-            description: "Call Bonnie to check in",
-            completed: true
-        },
-        {
-            description: "Read through coding notes",
-            completed: true
-        }
-    ], (error, result) => {
-        if (error) return console.log("Unable to insert documents.");
+    db.collection("tasks").find({completed: false}).toArray((error, users) => {
+        if (error) return console.log("Unable to fetch user");
 
-        console.log(result.ops);
-    }) */
+        console.log(users);
+    })
+
 });
