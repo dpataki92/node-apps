cd3cca458130008cf2142539c7eff22e579c677e
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-02 20:20:03 +0000

    add to update / delete and count resources with async-await

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -10,4 +10,15 @@ Task.findByIdAndDelete("5ff0b0d034fe240685fdafa6").then((resp) => {
 })
 .catch((e) => {
     console.log(e);
-})
+});
+
+const deleteTaskAndCount = async(id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed: false});
+
+    return count;
+}
+
+deleteTaskAndCount("5ff0afec2a72c7067b45d61a")
+.then((result) => console.log(result))
+.catch((e) => {console.log(e)});
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -9,3 +9,14 @@ User.findByIdAndUpdate("5ff088c099e05d041f7f0338", {age: 29}).then((user) => {
 })
 .then((result) => {console.log(result)})
 .catch((e) => {console.log(e)});
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age});
+    const count = await User.countDocuments({age});
+    return count;
+}
+
+updateAgeAndCount("5ff067050efc1303d1963d12", 45).then((count) => {
+    console.log(count);
+})
+.catch((e) => console.log(e));
