6e4f20af039895f652d536ea2e37424c36c57e6d
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-02 18:53:16 +0000

    add example - promise-chaining

--- playground/7-promises.js
+++ playground/7-promises.js
@@ -1,14 +1,30 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve([1,4,7]);
-        // reject("Things went wrong")
-    }, 2000)
-});
+const add = (a,b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a+b);
+        }, 2000);
+    })
+}
 
-doWorkPromise
-.then((result) => {
-    console.log('Success', result);
+/* add(1,2).then((sum) => {
+    console.log(sum);
+
+    add(sum, 5)
+    .then((sum2) => {console.log(sum2)})
+    .catch((e) => console.log(e));
 })
-.catch((error) => {
-    console.log("Failure", error)
+.catch((e) => console.log(e)); */
+
+add(2,2)
+.then((sum) => {
+    console.log(sum);
+    return add(sum, 6);
 })
+.then((sum2) => {
+    console.log(sum2);
+    return add(sum2, 10);
+})
+.then((sum3) => {
+    console.log(sum3);
+})
+.catch((e) => {console.log(e)});
