5631cb61b6d5db264ada36ba81fba032b6f27ca1
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-02 17:44:55 +0000

    create addpoints for creating users and tasks

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     useNewUrlParser: true, 
@@ -7,68 +6,4 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     useCreateIndex: true
 });
 
-const User = mongoose.model("User", {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Email is invalid!");
-            }
-        },
-        trim: true,
-        lowercase: true
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error("Age must be a positive number!");
-            }
-        }
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: true,
-        minLength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes("password")) {
-                throw new Error("Password cannot contain word 'password'!");
-            }
-        }
-    }
-});
-
-/* const me = new User({name: "Greg", email: "    GREG@gmail.com     ", password: "hello0102"});
-me.save()
-.then(() => {console.log(me)})
-.catch((error) => {console.log("Error! ", error)}) */
-
-const Task = mongoose.model("Task", {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
-
-const task = new Task({description: "   Clean the house  "});
-task.save().then(() => console.log(task)).catch((error)=> {console.log(error)})
-
-/* const me = new User({name: "something", age: "Mike"});
-
-me.save()
-.then(() => {console.log(me)})
-.catch((error) => {console.log("Error! ", error)}) */
 
