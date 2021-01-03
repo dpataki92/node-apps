4e8572b4d9c07c5f9ebf721fbcfd4353640c3b2d
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-03 19:14:31 +0000

    add authentication flow to signup route

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -7,8 +7,9 @@ router.post("/users", async (req, res) => {
     const user = new User(req.body);
 
     try {
-        await user.save();
-        res.status(201).send(user);
+        await user.save()
+        const token = await user.generateAuthToken();
+        res.send({user, token});
     }
     catch(e) {
         res.status(400).send(e);
