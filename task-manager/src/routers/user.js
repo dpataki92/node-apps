a135392cfe2471f92a981ba9f1a3276fd01a4bd1
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-05 14:32:18 +0000

    add logout route

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -28,12 +28,36 @@ router.post("/users/login", async (req, res) => {
     }   
 });
 
+router.post("/users/logout", auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
+        await req.user.save();
+
+        res.send();
+    }
+    catch(e) {
+        res.status(500).send();
+    }   
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+
+        res.send();
+    }
+    catch(e) {
+        res.status(500).send();
+    }   
+});
+
 router.get("/users/me", auth, async (req, res) => {
     res.send(req.user);
     
 });
 
-router.get("/users/:id", async (req, res) => {
+router.get("/users/:id", auth, async (req, res) => {
     const _id = req.params.id;
 
      try {
