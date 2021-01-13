c8b657b457b1c9d04679ea4addf2d667c41f9bf4
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-13 18:15:10 +0100

    setup socket.io connection

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 
@@ -10,6 +15,10 @@ const publicDirectoryPath = path.join( __dirname, "../public");
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}.`)
+io.on("connection", () => {
+    console.log("new web socket connection");
 })
+
+server.listen(port, () => {
+    console.log(`Server is up and running on port ${port}.`)
+});
