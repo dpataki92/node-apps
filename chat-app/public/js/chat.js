4f2223443fbfebe4c4806b4de4968b43c6b25cb9
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-14 20:51:28 +0100

    add automatic scrolling

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -4,7 +4,6 @@ const socket = io();
 const $messageForm = document.getElementById("message-form");
 const $messageFormInput = $messageForm.querySelector("input");
 const $messageFormButton = $messageForm.querySelector("button");
-
 const $sendLocationButton = document.getElementById("send-location");
 const $messages = document.getElementById("messages");
 
@@ -16,6 +15,29 @@ const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
+const autoscroll = () => {
+    // new message element
+    const $newMessage = $messages.lastElementChild;
+
+    // height of the new message
+    const newMessageStyles = getComputedStyle($newMessage);
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight;
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight;
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight <= scrollOffset + 10) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
 socket.on("message", (msg) => {
     const html = Mustache.render(messageTemplate, {
         username: msg.username,
@@ -23,6 +45,7 @@ socket.on("message", (msg) => {
         createdAt: moment(msg.createdAt).format("h:mm a")
     });
     $messages.insertAdjacentHTML("beforeend", html);
+    autoscroll()
 });
 
 socket.on("locationMessage", (locationMessage) => {
@@ -32,6 +55,7 @@ socket.on("locationMessage", (locationMessage) => {
         createdAt: moment(locationMessage.createdAt).format("h:mm a")
     });
     $messages.insertAdjacentHTML("beforeend", html);
+    autoscroll()
 });
 
 socket.on("roomData", ( {room, users} ) => {
